24f96c1fbd6a9261d98b7a33c2c0a264362b031b
aikosogabe <aiko.sogabe@fuller.co.jp>
2021-12-15 14:18:58 +0900

    reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
